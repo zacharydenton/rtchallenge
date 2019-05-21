761f2b71d3c293d81f907d6b8d4c66795fded9c6
Zach Denton <z@chdenton.com>
2019-05-20 19:27:19 -0700

    Implement hit detection with arbitrary intersections.

--- src/object.rs
+++ src/object.rs
@@ -11,15 +11,17 @@ pub fn sphere() -> Object {
     Object::Sphere {}
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Intersection<'a> {
     pub t: f32,
     pub object: &'a Object,
 }
 
+type Intersections<'a> = Vec<Intersection<'a>>;
+
 impl Object {
     /// Returns the collection of Intersections where the ray intersects the object.
-    pub fn intersect(&self, ray: Ray) -> Option<(Intersection, Intersection)> {
+    pub fn intersect(&self, ray: Ray) -> Intersections {
         match *self {
             Object::Sphere {} => {
                 // The vector from the sphere's center, to the ray origin
@@ -33,11 +35,11 @@ impl Object {
                 let discriminant = b * b - 4. * a * c;
 
                 if discriminant < 0. {
-                    None
+                    vec![]
                 } else {
                     let t1 = (-b - discriminant.sqrt()) / (2. * a);
                     let t2 = (-b + discriminant.sqrt()) / (2. * a);
-                    Some((
+                    vec![
                         Intersection {
                             t: t1,
                             object: self,
@@ -46,11 +48,19 @@ impl Object {
                             t: t2,
                             object: self,
                         },
-                    ))
+                    ]
                 }
             }
         }
     }
+}
+
+pub fn hit(intersections: Intersections) -> Option<Intersection> {
+    intersections
+        .iter()
+        .filter(|i| i.t >= 0.)
+        .min_by(|a, b| a.t.partial_cmp(&b.t).unwrap())
+        .map(|&i| i)
 }
 
 #[cfg(test)]
@@ -66,7 +76,7 @@ mod tests {
         let r = ray(point3(0., 0., -5.), vector3(0., 0., 1.));
         let s = sphere();
         let xs = s.intersect(r);
-        assert_eq!(xs, Some((intersection(4.0, &s), intersection(6.0, &s))));
+        assert_eq!(xs, vec![intersection(4.0, &s), intersection(6.0, &s)]);
     }
 
     #[test]
@@ -74,7 +84,7 @@ mod tests {
         let r = ray(point3(0., 1., -5.), vector3(0., 0., 1.));
         let s = sphere();
         let xs = s.intersect(r);
-        assert_eq!(xs, Some((intersection(5.0, &s), intersection(5.0, &s))));
+        assert_eq!(xs, vec![intersection(5.0, &s), intersection(5.0, &s)]);
     }
 
     #[test]
@@ -82,7 +92,7 @@ mod tests {
         let r = ray(point3(0., 2., -5.), vector3(0., 0., 1.));
         let s = sphere();
         let xs = s.intersect(r);
-        assert_eq!(None, xs);
+        assert_eq!(xs, vec![]);
     }
 
     #[test]
@@ -90,7 +100,7 @@ mod tests {
         let r = ray(point3(0., 0., 0.), vector3(0., 0., 1.));
         let s = sphere();
         let xs = s.intersect(r);
-        assert_eq!(xs, Some((intersection(-1.0, &s), intersection(1.0, &s))));
+        assert_eq!(xs, vec![intersection(-1.0, &s), intersection(1.0, &s)]);
     }
 
     #[test]
@@ -98,6 +108,48 @@ mod tests {
         let r = ray(point3(0., 0., 5.), vector3(0., 0., 1.));
         let s = sphere();
         let xs = s.intersect(r);
-        assert_eq!(xs, Some((intersection(-6.0, &s), intersection(-4.0, &s))));
+        assert_eq!(xs, vec![intersection(-6.0, &s), intersection(-4.0, &s)]);
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_positive_t() {
+        let s = sphere();
+        let i1 = intersection(1., &s);
+        let i2 = intersection(2., &s);
+        let xs = vec![i1, i2];
+        let i = hit(xs);
+        assert_eq!(i, Some(i1));
+    }
+
+    #[test]
+    fn the_hit_when_some_intersections_have_negative_t() {
+        let s = sphere();
+        let i1 = intersection(-1., &s);
+        let i2 = intersection(1., &s);
+        let xs = vec![i2, i1];
+        let i = hit(xs);
+        assert_eq!(i, Some(i2));
+    }
+
+    #[test]
+    fn the_hit_when_all_intersections_have_negative_t() {
+        let s = sphere();
+        let i1 = intersection(-2., &s);
+        let i2 = intersection(-1., &s);
+        let xs = vec![i2, i1];
+        let i = hit(xs);
+        assert_eq!(i, None);
+    }
+
+    #[test]
+    fn the_hit_is_always_the_lowest_nonnegative_intersection() {
+        let s = sphere();
+        let i1 = intersection(5., &s);
+        let i2 = intersection(7., &s);
+        let i3 = intersection(-3., &s);
+        let i4 = intersection(2., &s);
+        let xs = vec![i1, i2, i3, i4];
+        let i = hit(xs);
+        assert_eq!(i, Some(i4));
     }
 }

5947ec5fe8fe1f05edd87b014ab1f303a09380a9
Zach Denton <z@chdenton.com>
2019-05-20 17:01:32 -0700

    Add clock program.

---
+++ src/bin/clock.rs
@@ -0,0 +1,27 @@
+extern crate rtchallenge;
+use rtchallenge::canvas::*;
+use rtchallenge::color::*;
+use rtchallenge::ppm::*;
+use rtchallenge::transform::*;
+use rtchallenge::tuple::*;
+
+fn main() {
+    let mut canvas = canvas(1000, 1000);
+    let color = color(1.0, 1.0, 1.0);
+    let transform = rotate_z(std::f32::consts::PI / 6.);
+    let mut point = point3(0.0, 0.8, 0.0);
+
+    for _ in 0..12 {
+        point = transform * point;
+        let x = (canvas.width as f32 / 2. + point.x * canvas.width as f32 / 2.).round() as usize;
+        let y = (canvas.height as f32 / 2. - point.y * canvas.height as f32 / 2.).round() as usize;
+        canvas.set_color(x - 1, y, &color);
+        canvas.set_color(x, y, &color);
+        canvas.set_color(x + 1, y, &color);
+        canvas.set_color(x, y - 1, &color);
+        canvas.set_color(x, y, &color);
+        canvas.set_color(x, y + 1, &color);
+    }
+
+    print!("{}", canvas_to_ppm(canvas));
+}

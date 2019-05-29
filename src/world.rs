f5b52651ed93a2aea19c21c0ef84697a420f7440
Zach Denton <z@chdenton.com>
2019-05-28 21:03:46 -0700

    Add world-ray shading.

--- src/world.rs
+++ src/world.rs
@@ -42,17 +42,46 @@ impl World {
             .objects
             .iter()
             .flat_map(|obj| obj.intersect(ray))
+            .filter(|i| i.t >= 0.)
             .collect::<Intersections>();
 
         intersections.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
 
         intersections
     }
+
+    /// Returns the color at the given intersection.
+    pub fn shade(&self, intersection: &Intersection) -> Color {
+        self.lights
+            .iter()
+            .map(|light| {
+                intersection.object.material.lighting(
+                    light,
+                    &intersection.point.unwrap(),
+                    &intersection.eyev.unwrap(),
+                    &intersection.normalv.unwrap(),
+                )
+            })
+            .fold(color(0., 0., 0.), |acc, x| acc + x)
+    }
+
+    /// Intersects the ray with the world and returns the color at the resulting
+    /// intersection.
+    pub fn color_at(&self, ray: &Ray) -> Color {
+        let intersections = self.intersect(ray);
+
+        if intersections.len() == 0 {
+            return color(0., 0., 0.);
+        }
+
+        self.shade(&intersections[0])
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use assert_approx_eq::assert_approx_eq;
     use test::Bencher;
 
     #[test]
@@ -81,10 +110,80 @@ mod tests {
         assert_eq!(xs[3].t, 6.0);
     }
 
+    #[test]
+    fn shading_an_intersection() {
+        let w = default_world();
+        let r = ray(point3(0., 0., -5.), vector3(0., 0., 1.));
+        let xs = w.intersect(&r);
+        let i = xs.iter().find(|x| x.t == 4.).unwrap();
+        let c = w.shade(&i);
+
+        assert_approx_eq!(c.r, 0.38066, 1e-5);
+        assert_approx_eq!(c.g, 0.47583, 1e-5);
+        assert_approx_eq!(c.b, 0.2855, 1e-5);
+    }
+
+    #[test]
+    fn shading_an_intersection_from_the_inside() {
+        let mut w = default_world();
+        w.lights = vec![point_light(point3(0., 0.25, 0.), color(1., 1., 1.))];
+        let r = ray(point3(0., 0., 0.), vector3(0., 0., 1.));
+        let xs = w.intersect(&r);
+        let i = xs.iter().find(|x| x.t == 0.5).unwrap();
+        let c = w.shade(&i);
+
+        assert_approx_eq!(c.r, 0.90498, 1e-5);
+        assert_approx_eq!(c.g, 0.90498, 1e-5);
+        assert_approx_eq!(c.b, 0.90498, 1e-5);
+    }
+
+    #[test]
+    fn the_color_when_a_ray_misses() {
+        let w = default_world();
+        let r = ray(point3(0., 0., -5.), vector3(0., 1., 0.));
+        let c = w.color_at(&r);
+
+        assert_approx_eq!(c.r, 0.0, 1e-5);
+        assert_approx_eq!(c.g, 0.0, 1e-5);
+        assert_approx_eq!(c.b, 0.0, 1e-5);
+    }
+
+    #[test]
+    fn the_color_when_a_ray_hits() {
+        let w = default_world();
+        let r = ray(point3(0., 0., -5.), vector3(0., 0., 1.));
+        let c = w.color_at(&r);
+
+        assert_approx_eq!(c.r, 0.38066, 1e-5);
+        assert_approx_eq!(c.g, 0.47583, 1e-5);
+        assert_approx_eq!(c.b, 0.2855, 1e-5);
+    }
+
+    #[test]
+    fn the_color_with_an_intersection_behind_the_ray() {
+        let mut w = default_world();
+        w.objects[0].material.ambient = 1.0;
+        w.objects[1].material.ambient = 1.0;
+
+        let r = ray(point3(0., 0., 0.75), vector3(0., 0., -1.));
+        let c = w.color_at(&r);
+
+        assert_eq!(c, w.objects[1].material.color);
+    }
+
     #[bench]
     fn bench_intersect_a_world_with_a_ray(bencher: &mut Bencher) {
         let w = default_world();
         let r = ray(point3(0., 0., -5.), vector3(0., 0., 1.));
         bencher.iter(|| w.intersect(&r));
+    }
+
+    #[bench]
+    fn bench_shading_an_intersection(bencher: &mut Bencher) {
+        let w = default_world();
+        let r = ray(point3(0., 0., -5.), vector3(0., 0., 1.));
+        let xs = w.intersect(&r);
+        let i = xs.iter().find(|x| x.t == 4.).unwrap();
+        bencher.iter(|| w.shade(&i));
     }
 }
